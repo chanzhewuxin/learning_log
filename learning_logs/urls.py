46bae5017338f9ab3bc5ba4c52a39d9e0066ddd4
paramita <cpeng0907@163.com>
2018-11-09 20:04:18 +0800

    熟悉添加页面，创建模板

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+"""定义learning_logs的URL模式"""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns=[
+    # 主页
+    url(r'^$',views.index,name='index'),
+    # 显示所有的主题
+    url(r'^topics/$',views.topics,name='topics'),
+    url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
+]

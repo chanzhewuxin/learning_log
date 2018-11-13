da91500f809c5a820e47f1ceeaf9e4fb3f80b25d
paramita <cpeng0907@163.com>
2018-11-13 19:54:11 +0800

    20181113

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,7 @@ urlpatterns=[
     # 显示所有的主题
     url(r'^topics/$',views.topics,name='topics'),
     url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
+    url(r'^new_topic/$',views.new_topic,name='new_topic'),
+    url(r'^new_entry/(?P<topic_id>\d+)/$',views.new_entry,name='new_entry'),
+    url(r'^edit_entry/(?P<entry_id>\d+)/$',views.edit_entry,name='edit_entry'),
 ]
